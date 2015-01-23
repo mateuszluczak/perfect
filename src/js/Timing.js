7936b6ec47e6ca9564ab69b8a3f574a6df7671a9
ciapunek <ciapunek@gmail.com>
2015-01-23 19:06:25 +0100

    Minor fix

--- src/js/Timing.js
+++ src/js/Timing.js
@@ -6,29 +6,29 @@
     var firstPaint = window.chrome.loadTimes().firstPaintTime * 1000;
 
     // Time to first paint
-    api.firstPaintTime = Math.round(firstPaint - (window.chrome.loadTimes().startLoadTime*1000));
+    api.firstPaintTime = Math.round(0, firstPaint - (window.chrome.loadTimes().startLoadTime*1000));
     // Total time from start to load
-    api.loadTime = timing.loadEventEnd - timing.navigationStart;
+    api.loadTime = Math.max(0, timing.loadEventEnd - timing.navigationStart);
     // Time spent constructing the DOM tree
-    api.domReadyTime = timing.domComplete - timing.domInteractive;
+    api.domReadyTime =  Math.max(0, timing.domComplete - timing.domInteractive);
     // Time consumed preparing the new page
-    api.readyStart = timing.fetchStart - timing.navigationStart;
+    api.readyStart = Math.max(0, timing.fetchStart - timing.navigationStart);
     // Time spent during redirection
-    api.redirectTime = timing.redirectEnd - timing.redirectStart;
+    api.redirectTime = Math.max(0, timing.redirectEnd - timing.redirectStart);
     // AppCache
-    api.appcacheTime = timing.domainLookupStart - timing.fetchStart;
+    api.appcacheTime = Math.max(0, timing.domainLookupStart - timing.fetchStart);
     // Time spent unloading documents
-    api.unloadEventTime = timing.unloadEventEnd - timing.unloadEventStart;
+    api.unloadEventTime = Math.max(0, timing.unloadEventEnd - timing.unloadEventStart);
     // DNS query time
-    api.lookupDomainTime = timing.domainLookupEnd - timing.domainLookupStart;
+    api.lookupDomainTime = Math.max(0, timing.domainLookupEnd - timing.domainLookupStart);
     // TCP connection time
-    api.connectTime = timing.connectEnd - timing.connectStart;
+    api.connectTime = Math.max(0, timing.connectEnd - timing.connectStart);
     // Time spent during the request
-    api.requestTime = timing.responseEnd - timing.requestStart;
+    api.requestTime = Math.max(0, timing.responseEnd - timing.requestStart);
     // Request to completion of the DOM loading
-    api.initDomTreeTime = timing.domInteractive - timing.responseEnd;
+    api.initDomTreeTime = Math.max(0, timing.domInteractive - timing.responseEnd);
     // Load event time
-    api.loadEventTime = timing.loadEventEnd - timing.loadEventStart;
+    api.loadEventTime = Math.max(0, timing.loadEventEnd - timing.loadEventStart);
 
     return api;
 })();
